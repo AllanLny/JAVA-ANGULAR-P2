9e36ffefa8da9e0b8e8818c346865be7aaf34ff9
AllanLny <allan1411lannoy@gmail.com>
2024-11-10 18:44:12 +0100

    fix: all any

--- src/app/component/piechart/piechart.component.ts
+++ src/app/component/piechart/piechart.component.ts
@@ -1,11 +1,9 @@
-import { Component, OnInit , HostListener } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { NgxChartsModule, Color, ScaleType } from '@swimlane/ngx-charts';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { Router } from '@angular/router';
 import { Observable, map } from 'rxjs';
 import { OlympicCountry } from 'src/app/core/models/Olympic';
-
-
 
 @Component({
   selector: 'app-piechart',
@@ -30,14 +28,14 @@ export class PiechartComponent implements OnInit {
 
   constructor(private olympicService: OlympicService, private router: Router) {
     this.updateChartSize();
-    this.tooltipX = 0; 
-    this.tooltipY = 0; 
+    this.tooltipX = 0;
+    this.tooltipY = 0;
   }
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
     this.graphData$ = this.olympics$.pipe(
-      map((olympics) => {
+      map((olympics: OlympicCountry[]) => {
         if (!olympics) {
           return [];
         }
@@ -50,20 +48,20 @@ export class PiechartComponent implements OnInit {
       })
     );
     this.updateChartSize();
-    
   }
-    @HostListener('window:resize', ['$event'])
-    onResize(event: any): void {
-      this.updateChartSize();
-    }
-  
-    updateChartSize(): void {
-      const width = window.innerWidth;
-      const heightRatio = width <= 740 ? 0.37 : 0.79;
-      this.view = [width * 0.79, window.innerHeight * heightRatio];
-    }
 
-    onClick(data: any): void {
-      this.router.navigate(['/detail', data.name]);
-    }
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.updateChartSize();
   }
+
+  updateChartSize(): void {
+    const width = window.innerWidth;
+    const heightRatio = width <= 740 ? 0.37 : 0.79;
+    this.view = [width * 0.79, window.innerHeight * heightRatio];
+  }
+
+  onClick(data: { name: string }): void {
+    this.router.navigate(['/detail', data.name]);
+  }
+}
